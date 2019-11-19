b27f7b1a5702a91e70beb858df2557a1129b8729
fabiengoyhex <fabien.goyhex@gmail.com>
2019-11-19 12:48:36 +0100

    added state in app.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,54 @@ import {Header} from './component'
 
 
 class App extends Component {
+  state={
+    loading:false,
+    movies : [
+  {
+    backdrop_path: "./images/Fast_large.jpg",
+    id: 475557,
+    overview:
+      "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
+    poster_path: "./images/Fast_large.jpg",
+    title: "Joker"
+  },
+   {
+    backdrop_path: "./images/Fast_large.jpg",
+    id: 475557,
+    overview:
+      "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
+    poster_path: "./images/Fast_large.jpg",
+    title: "Joker"
+  },{
+    backdrop_path: "./images/Fast_large.jpg",
+    id: 475557,
+    overview:
+      "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
+    poster_path: "./images/Fast_large.jpg",
+    title: "Joker"
+  },{
+    backdrop_path: "./images/Fast_large.jpg",
+    id: 475557,
+    overview:
+      "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
+    poster_path: "./images/Fast_large.jpg",
+    title: "Joker"
+  }
+],
+    badge : 0,
+    image : './images/Fast_large.jpg',
+    mTitle : 'Fast and Furious',
+    mDesc : `Fast and Furious ou Rapides et dangereux au Québec est une série de films américains d'action dont le premier est sorti en 2001. Elle comprend neuf longs métrages (sortis) ainsi que deux courts métrages. Le premier film est basé sur un article intitulé Racer X, écrit par le journaliste Ken Li et publié dans Vibe, parlant de courses-poursuites et de différents vols dans les rues de New-York (le film se déroule cependant à Los Angeles).`,
+    activePage:0,
+    totalPages:0,
+    searchText:''
+  }
   render(){
      return (
     <div className="App">
-      <Header badge={15}/>
-      <Home/>
+      <Header badge={this.state.badge}/>
+      <Home
+      {...this.state}/>
     
     </div>
   );
--- src/route/Home.js
+++ src/route/Home.js
@@ -2,55 +2,24 @@ import React, {Component} from "react"
 
 import {HeaderImg, SearchBar, PosterList, LoadButton} from '../component'
 
- const movies = [
-  {
-    backdrop_path: "./images/Fast_large.jpg",
-    id: 475557,
-    overview:
-      "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
-    poster_path: "./images/Fast_large.jpg",
-    title: "Joker"
-  },
-   {
-    backdrop_path: "./images/Fast_large.jpg",
-    id: 475557,
-    overview:
-      "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
-    poster_path: "./images/Fast_large.jpg",
-    title: "Joker"
-  },{
-    backdrop_path: "./images/Fast_large.jpg",
-    id: 475557,
-    overview:
-      "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
-    poster_path: "./images/Fast_large.jpg",
-    title: "Joker"
-  },{
-    backdrop_path: "./images/Fast_large.jpg",
-    id: 475557,
-    overview:
-      "Dans les annÈes 1980, ‡ Gotham City, Arthur Fleck, un humoriste de stand-up ratÈ, bascule dans la folie et devient le Joker.",
-    poster_path: "./images/Fast_large.jpg",
-    title: "Joker"
-  }
-];
 
 
 
 class Home extends Component{
     
     render(){
+      const { mTitle, mDesc, image, movies, loading } = this.props
         return(
             <div>
                 <HeaderImg
-                title ='fast and furious'
-                overview = 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum '
-                imgSrc = {`./images/Fast_large.jpg`}
+                title ={mTitle}
+                overview = {mDesc}
+                imgSrc = {image}
 
                 />
                 <SearchBar/>
                 <PosterList movies={movies}/>
-                <LoadButton loading ={false}/>
+                <LoadButton loading ={loading}/>
             </div>
         )
     }
